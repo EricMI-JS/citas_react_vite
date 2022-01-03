610ebc0c2f3143767c31074df6d66d0a454ed52f
Eric Morales <ericdaniel3103@gmail.com>
2022-01-02 22:24:09 -0600

    Creando un componente Errores

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Error from "./Error";
 
 function Formulario({ pacientes, setPacientes }) {
   const [nombre, setNombre] = useState("");
@@ -49,11 +50,7 @@ function Formulario({ pacientes, setPacientes }) {
         onSubmit={handleSubmit}
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
       >
-        {error && (
-          <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
-            <p>Todos los campos son obligatorios</p>
-          </div>
-        )}
+        {error && <Error mensaje="Todos los campo son obligatorios" />}
         <div className="mb-5">
           <label
             htmlFor="mascota"
