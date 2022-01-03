395dfba81cd88bd27592d832b58b21866517864c
Eric Morales <ericdaniel3103@gmail.com>
2022-01-02 21:53:27 -0600

    Validando el formulario con alerta

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,9 +7,16 @@ function Formulario() {
   const [fecha, setFecha] = useState("");
   const [sintomas, setSintomas] = useState("");
 
+  const [error, setError] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Enviando formulario...");
+
+    if ([nombre, propietario, email, fecha, sintomas].includes("")) {
+      setError(true);
+      return;
+    }
+    setError(false);
   };
 
   return (
@@ -24,6 +31,11 @@ function Formulario() {
         onSubmit={handleSubmit}
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
       >
+        {error && (
+          <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+            <p>Todos los campos son obligatorios</p>
+          </div>
+        )}
         <div className="mb-5">
           <label
             htmlFor="mascota"
