e8605b2eec8cf8a232baa205a88b85b7ce9eebe1
Eric Morales <ericdaniel3103@gmail.com>
2022-01-05 15:59:10 -0600

    Texto condicional para editar o agregar paciente

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -160,7 +160,7 @@ function Formulario({ pacientes, paciente, setPacientes }) {
         <input
           type="submit"
           className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all"
-          value="Agregar Paciente"
+          value={paciente.id ? "Editar Paciente" : "Agregar Paciente"}
         />
       </form>
     </div>
