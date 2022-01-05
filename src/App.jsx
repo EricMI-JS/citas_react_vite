5117c2880a1a01fe26f00c7eaeb413f39ecedc75
Eric Morales <ericdaniel3103@gmail.com>
2022-01-05 13:21:58 -0600

    Iterando las citas con .map

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
       <div className="mt-12 md:flex">
         <Formulario pacientes={pacientes} setPacientes={setPacientes} />
-        <ListadoPacientes />
+        <ListadoPacientes pacientes={pacientes} />
       </div>
     </div>
   );
